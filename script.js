527ca5eba00c7c49c0d07d1c5ebd556229a25c35
okaygay <130615422+okaygay@users.noreply.github.com>
2023-09-04 00:15:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ class CreateParticles {
 
 		this.data = {
 
-			text: '     Undex\nDvelobed By \n     _00h',
+			text: '     Undex\nDeveloped By \n     _00h',
 			amount: 1500,
 			particleSize: 1,
 			particleColor: 0xffffff,
